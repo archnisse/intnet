ecb8c52ca9331fbb1b121070742efd4ad6674c91
Ella Syk <ellas@kth.se>
2015-02-23 16:56:58 +0100

    Version 2 complete

--- Lab3/v2/lab3.js
+++ Lab3/v2/lab3.js
@@ -1,18 +1,31 @@
 $(document).ready(function(){
+	var chosen = [""];
 	for (i = 1; i <= 9; i++) {
 		for (j = 1; j <= 9; j++) {
 			$("#squareBox").append("<div id="+i+";"+j+" class='square'></div>");
+			if (Math.random() > 0.8) {
+				chosen[chosen.length] = ""+i+";"+j+"";
+			}
 		}
 		$("#squareBox").append("<div style='clear: both;'></div>");
 	}
 
  $(".square").click(function(){
+	var set = false;
+	for (i = 0; i < chosen.length; i++) {
+		if (chosen[i] == $(this).attr("id")) {
+			(this).style.background = "red";
+			set = true;
+		}
+	}
         document.getElementById("textInBox").innerHTML = $(this).attr("id");
 	/*if ($("#textBox").attr("id")) !=0 {
 		$("#1").replaceWith(
 	}
 	 $("#textBox").append("<br>");*/
-	(this).style.background = "green";
+	if (!set) {
+		(this).style.background = "green";
+	}
  });
 });
 
