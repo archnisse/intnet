458ded7af1e779598bd646d33fcec89d53088250
Nisse <viktorbj@kth.se>
2015-02-23 23:37:50 +0100

    There is now a matrix representing the grid with fields for the chosen and selected ones.

--- Lab3/v3/lab3.js
+++ Lab3/v3/lab3.js
@@ -1,15 +1,16 @@
 $(document).ready(function(){
-	var chosen = [""];
-	//var grid = [];
 	
 	var size = 9;
+	var grid = new Array(size);;
+	
 	
 	for (i = 1; i <= size; i++) {
-		//grid[i] = new Array(size);
+		grid[i] = new Array(size);
 		for (j = 1; j <= size; j++) {
 			$("#squareBox").append("<div id="+i+";"+j+" class='square'></div>");
+			grid[i][j] = {chosen: false, selected: false};
 			if (Math.random() > 0.8) {
-				chosen[chosen.length] = ""+i+";"+j+"";
+				grid[i][j].chosen = true;
 			}
 		}
 		$("#squareBox").append("<div style='clear: both;'></div>");
@@ -17,20 +18,15 @@ $(document).ready(function(){
 
  $(".square").click(function(){
 	var set = false;
-	for (i = 0; i < chosen.length; i++) {
-		if (chosen[i] == $(this).attr("id")) {
-			(this).style.background = "red";
-			set = true;
-		}
-	}
-        document.getElementById("textInBox").innerHTML = $(this).attr("id");
-	/*if ($("#textBox").attr("id")) !=0 {
-		$("#1").replaceWith(
-	}
-	 $("#textBox").append("<br>");*/
-	if (!set) {
+	var id = $(this).attr("id");
+	var x = id.split(";")[0];
+	var y = id.split(";")[1];
+	if (grid[x][y].chosen) {
+		(this).style.background = "red";
+	} else {
 		(this).style.background = "green";
 	}
+    document.getElementById("textInBox").innerHTML = $(this).attr("id");
  });
 });
 
